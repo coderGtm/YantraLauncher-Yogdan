58abac7431781971c344df53b2c7acbc6dd459dc
Gautam Mehta <66418526+coderGtm@users.noreply.github.com>
2023-09-27 13:55:12 +0530

    Create sendText.kt

---
+++ commands/sendText.kt
@@ -0,0 +1,10 @@
+// This is an example to show how you can define your commands
+
+// The function broadcasts a text message
+private fun sendText(msg: String) {
+        val  intent = Intent(Intent.ACTION_SEND)
+        intent.putExtra(Intent.EXTRA_TEXT, msg.trim())
+        intent.type = "text/plain"
+        startActivity(Intent.createChooser(intent, "Send via"))
+        printToConsole("Text broadcasted",6)
+}
