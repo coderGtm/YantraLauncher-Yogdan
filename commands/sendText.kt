846be6eebb94722e980913c841a834752289a2bb
Gautam Mehta <66418526+coderGtm@users.noreply.github.com>
2023-09-29 13:59:10 +0530

    Update sendText.kt

--- commands/sendText.kt
+++ commands/sendText.kt
@@ -6,5 +6,5 @@ private fun sendText(msg: String) {
         intent.putExtra(Intent.EXTRA_TEXT, msg.trim())
         intent.type = "text/plain"
         startActivity(Intent.createChooser(intent, "Send via"))
-        printToConsole("Text broadcasted",6)
+        printToConsole("Text broadcasted",OUTPUT_COLOR_SUCCESS)
 }
